a447638bdef8bce7ecd9fe0a0686d38faf23b342
moujie1998 <1659411502@qq.com>
2019-12-18 17:41:59 +0800

    后台初始化

---
+++ api/coachTicket/src/main/java/com/sixgod/coachTicket/CoachTicketApplication.java
@@ -0,0 +1,13 @@
+package com.sixgod.coachTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachTicketApplication.class, args);
+	}
+
+}
---
+++ api/coachTicket/src/test/java/com/sixgod/coachTicket/CoachTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixgod.coachTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
